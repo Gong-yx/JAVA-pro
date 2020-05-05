c50b6b8e36d8f47fd30fae72be12d48921bb65eb
Gong-yx <64535135+Gong-yx@users.noreply.github.com>
2020-05-05 08:28:36 +0800

    Update ToolButtons.java

--- ToolButtons.java
+++ ToolButtons.java
@@ -2,6 +2,9 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 
 public class ToolButtons extends JPanel {
     //设置右侧工具栏除数字九宫格外的所有按钮及其功能
@@ -9,7 +12,10 @@ public class ToolButtons extends JPanel {
     private JButton Delete;
     private JButton Tips;
     private JButton Quit;
-    JTextField textField=new JTextField(16);
+    public static int count=10;
+    private JButton Record;
+
+
 
     public ToolButtons(){
         //设置撤销按钮
@@ -20,18 +26,51 @@ public class ToolButtons extends JPanel {
         Tips = new JButton("提示");
         //设置退出按钮
         Quit = new JButton("退出");
-        setLayout(new GridLayout(4,1));
+        //
+        Record=new JButton("保存");
+        setLayout(new GridLayout(5,1));
         add(Tips);
+        Tips.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                OnTips();
+            }
+        });
+
+
         add(Delete);
+        Delete.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Grid.txtGame[Grid.Z][Grid.X][Grid.Y].setText("");
+            }
+        });
+
+        //添加撤销按钮到面板并设置其功能
         add(Repeal);
+        Repeal.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+        add(Record);
         //添加退出按钮到面板并设置其功能
         add(Quit);
+
         Quit.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 OnQuit();
             }
         });
+    }
+    //设置答案提示框的功能
+    public void OnTips(){
+        this.count--;
+    }
+
+    public static int getCount(){
+        return count;
     }
 
     public void OnQuit(){
