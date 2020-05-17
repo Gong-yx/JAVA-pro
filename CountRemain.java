da23cba911ba60ea432381d4798cf6631c377d7c
Gong-yx <64535135+Gong-yx@users.noreply.github.com>
2020-05-17 09:17:41 +0800

    Update CountRemain.java

--- CountRemain.java
+++ CountRemain.java
@@ -7,18 +7,18 @@ public class CountRemain extends JPanel {
     public static JTextField countRemains;
     public int count;
 
-//布局剩余提示次数在页面下方
+    //布局剩余提示次数在页面下方
     public CountRemain(){
-    sentence=new JLabel("剩余提示次数:");
-    countRemains=new JTextField();
-    count=ToolButtons.getCount();
-    countRemains.setText(String.valueOf(count));
-    setLayout(new GridLayout(1,2));
-    add(sentence);
-    add(countRemains);
-    countRemains.setEditable(false);
+        sentence=new JLabel("剩余提示次数:");
+        countRemains=new JTextField();
+        count=ToolButtons.getCount();
+        countRemains.setText(String.valueOf(count));
+        setLayout(new GridLayout(1,2));
+        add(sentence);
+        add(countRemains);
+        countRemains.setEditable(false);
     }
-//点击提示按钮，对应剩余提示次数减，页面上的显示改变
+    //点击提示按钮，对应剩余提示次数减，页面上的显示改变
     public static void setText(int count) {
         countRemains.setText(String.valueOf(count));
     }
